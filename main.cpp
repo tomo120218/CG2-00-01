dfbadc2dd5ac3f6477d5a54dd7ddc400a3a487ff
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2025-10-09 10:00:24 +0900

    エラー

--- main.cpp
+++ main.cpp
@@ -1050,6 +1050,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ModelData modelData = LoadObjFile("resources", "fence.obj");
 
 	//ModelData modelData = LoadObjFile("resources", "axis.obj");
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+
+	if (textureColor.a == 0.0) {
+		discard;
+	}
 
 	//頂点リソースを作る
 	ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
